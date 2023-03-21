9a5d165ee521811425b1cf18701888b9ff51cfe6
kitou88C39 <kanfutrooper@icloud.com>
2023-03-21 14:57:08 +0900

    bean/Motorcycle.javaを作成

--- motocatalog/src/main/bean/Motorcycle.java
+++ motocatalog/src/main/bean/Motorcycle.java
@@ -1,5 +1,29 @@
 package jp.co.planaria.sample.bean;
 
+import java.time.LocalDateTime;
+
 public class Motorcycle {
     
+    //バイク番号
+    private String motoNo;
+    //バイク名
+    private String motoName;
+    //シート高
+    private String seatHeight;
+    //シリンダー
+    private Integer cylinder;
+    //冷却
+    private Integer cooling;
+    //価格
+    private Integer price;
+    //バージョン
+    private Integer comment;
+    //ブランドID
+    private String brandId;
+    //バージョン
+    private Integer version;
+    //登録日時
+    private LocalDateTime insDt;
+    //更新日時
+    private LocalDateTime updDt;
 }
